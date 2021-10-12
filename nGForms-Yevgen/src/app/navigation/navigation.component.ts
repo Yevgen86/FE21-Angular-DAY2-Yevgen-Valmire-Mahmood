7802eafe070ef060298454e45d76dfd19eb09ad7
Yevgen Kutsy <y.kutsy86@gmail.com>
2021-10-12 14:31:24 +0200

    navigation btn for donation implemented and tested

--- nGForms-Yevgen/src/app/navigation/navigation.component.ts
+++ nGForms-Yevgen/src/app/navigation/navigation.component.ts
@@ -7,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  // Here we create a property for counter of clicks
+  // Here we create a property for countDonationOnClick method
   clickCounter:number = 0;
+  donationAmount:number = 0;
+  vipMessage:string = "";
 
   countDonationOnClick() {
-    this.clickCounter+10;
+    
+    this.clickCounter++;
+    this.donationAmount = this.clickCounter*10;
+
+    if(this.donationAmount >= 200) {
+      this.vipMessage = "***|_ Congratulation! Now you have status of VIP! _|***";
+    } 
   }
 
   constructor() { }
